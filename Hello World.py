a63c1eb8993b97964616c80b63884909d3053640
ProgrammerOutlaw123 <31458662+GalacticalEdge@users.noreply.github.com>
2022-02-24 18:00:05 -0800

    Day 4.1 Patch

--- Hello World.py
+++ Hello World.py
@@ -258,12 +258,24 @@ phonebook = {
 del phonebook["Scammer 1"]
 print(phonebook)
 
-print("We are exploring built-in modules")
-
-import foo.bar
-
-__init__.py
-
-__all__ = ["bar"]
-
 # Left off on Number Arrays
+
+height = [1.87, 1.87, 1.82, 1.91, 1.90, 1.85]
+weight = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
+
+import numpy as np
+
+np_height = np.array(height)
+np_weight = np.array(weight)
+
+print(type(np_height))
+
+bmi = np_weight / np_height ** 2
+
+print(bmi)
+
+bmi > 23
+
+bmi[bmi > 23]
+
+# Left off on Pandas Basics
