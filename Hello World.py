d7a6319ba328d10d5b1278f4d0497d5ca3a81dc2
ProgrammerOutlaw123 <31458662+GalacticalEdge@users.noreply.github.com>
2022-02-28 21:17:14 -0800

    Day 8 Patch
    Take a two day hiatus, but I covered one more section in my project. All I can do for tonight

--- Hello World.py
+++ Hello World.py
@@ -331,3 +331,25 @@ c = sum(a,b)
 print(c)
 
 # Left off on Multiple Function Arguments on learnpython.org
+
+def foo(first, second, third, fourth, fifth, *therest):
+    print("First: %s" %(first))
+    print("Second: %s" %(second))
+    print("Third: %s" %(third))
+    print("Fourth: %s" %(fourth))
+    print("Fifth: %s" %(fifth))
+    print("And all the rest... %s" %(list(therest)))
+
+foo(1, 2, 3, 4, 5)
+
+def bar(first, second, third, **options):
+    if options.get("action") == "sum":
+        print("The sum is: %d" %(first + second + third))
+
+        if options.get("number") == "first":
+            return first
+
+result = bar(1, 2, 3, action = "sum", number = "first")
+print("Result: %d" %(result))
+
+# Left off on Regular Expressions on learnpython.org
