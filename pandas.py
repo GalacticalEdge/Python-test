ce559054f974f7b229bec170bafee1b81a867aa5
ProgrammerOutlaw123 <31458662+GalacticalEdge@users.noreply.github.com>
2022-03-01 17:54:24 -0800

    Update 2
    I am getting closer, but I don't know why I'm getting a 'pd' is not defined error

--- pandas.py
+++ pandas.py
@@ -1,13 +1,14 @@
 print("This is pandas basics")
 
-def pandas():
-    import pandas as pd
+def my_function(pandas):
 
-dict = {"country": ["Brazil", "India", "China", "South Africa"], 
-        "capital": ["Brasilia", "New Dehli", "Beijing", "Pretoria"], 
-        "area": [8.516, 3.286, 9.597, 1.221], 
-        "population": [200.4, 1252, 1357, 52.98] 
-       }
+    dict = {"country": ["Brazil", "India", "China", "South Africa"],
+       "capital": ["Brasilia", "New Dehli", "Beijing", "Pretoria"],
+       "area": [8.516, 3.286, 9.597, 1.221],
+       "population": [200.4, 1252, 1357, 52.98] }
 
 brics = pd.DataFrame(dict)
 print(brics)
+
+def pandas():
+    import pandas as pd
